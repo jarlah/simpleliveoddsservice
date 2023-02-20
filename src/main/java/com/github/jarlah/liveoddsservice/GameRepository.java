1bae75b40f635c24ad47fca94ba316c6ca964cf9
Jarl André Hübenthal <jarlah@protonmail.com>
2023-02-20 15:20:27 +0100

    refactor

--- src/main/java/com/github/jarlah/liveoddsservice/GameRepository.java
+++
@@ -1,5 +0,0 @@
-package com.github.jarlah.liveoddsservice;
-
-public interface GameRepository {
-
-}
--- src/main/java/com/github/jarlah/liveoddsservice/Main.java
+++ src/main/java/com/github/jarlah/liveoddsservice/Main.java
@@ -4,23 +4,18 @@ import org.apache.commons.lang3.StringUtils;
 
 class Main {
     public static void main(String[] args) {
-        var scoreRepository = new ScoreRepositoryMemoryImpl();
-        var mexico = new Team("Mexico", 0);
-        var canada = new Team("Canada", 5);
-        scoreRepository.addScore(mexico, canada);
-        var spain = new Team("Spain", 10);
-        var brazil = new Team("Brazil", 2);
-        scoreRepository.addScore(spain, brazil);
-        var germany = new Team("Germany", 2);
-        var france = new Team("France", 2);
-        scoreRepository.addScore(germany, france);
-        var uruguay = new Team("Uruguay", 6);
-        var italy = new Team("Italy", 6);
-        scoreRepository.addScore(uruguay, italy);
-        var argentina = new Team("Argentina", 3);
-        var australia = new Team("Australia", 1);
-        scoreRepository.addScore(argentina, australia);
-        System.out.println(StringUtils.join(scoreRepository.getAllScores(), "\n"));
+        var repo = new ScoreRepositoryMemoryImpl();
+        addScore(repo, "Mexico", 0, "Canada", 5);
+        addScore(repo, "Spain", 10, "Brazil", 2);
+        addScore(repo, "Germany", 2, "France", 2);
+        addScore(repo, "Uruguay", 6, "Italy", 6);
+        addScore(repo, "Argentina", 3, "Australia", 1);
+        System.out.println(StringUtils.join(repo.getAllScores(), "\n"));
+    }
 
+    private static void addScore(ScoreRepository scoreRepository, String homeName, int homeScore, String awayName, int awayScore) {
+        var homeTeam = new Team(homeName, homeScore);
+        var awayTeam = new Team(awayName, awayScore);
+        scoreRepository.addScore(homeTeam, awayTeam);
     }
 }
